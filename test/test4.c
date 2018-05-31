42fd8ab3fddf439c8fd66a8036e1e3711715b51b
Latchesar Ionkov <lionkov@lanl.gov>
2018-05-31 09:31:58 -0600

    some more tests

--- test/test4.c
+++ test/test4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sicm_low.h>
+#include <unistd.h>
 
 #define N 100000
 
@@ -7,10 +8,10 @@ sicm_device_list devs;
 
 int main() {
 	int i;
-	sicm_arena s1, s2, s3;
+	sicm_arena s1, s2;
 	sicm_device *d1, *d2;
 	size_t pgsz;
-	char *buf1[N], *buf2;
+	char *buf1, *buf2;
 
 	devs = sicm_init();
 	d1 = &devs.devices[0];
@@ -33,38 +34,24 @@ int main() {
 		return -1;
 	}
 
-	s3 = sicm_arena_create(0, d2);
-	if (s3 == NULL) {
-		fprintf(stderr, "sarena_create failed\n");
-		return -1;
-	}
-
-	if (sicm_arena_alloc(s3, 8*1024*1024*1024LL) == NULL) {
+	if ((buf1 = sicm_arena_alloc(s2, 8*1024*1024*1024LL)) == NULL) {
 		fprintf(stderr, "huge alloc failed\n");
 		return -1;
 	}
 
-	for(int i = 0; i < N; i++) {
-		buf1[i] = sicm_arena_alloc(s1, 200);
-	}
-
-	if (sicm_arena_set_device(s1, d1) < 0) {
-		fprintf(stderr, "can't change memory policy\n");
+	if (sicm_arena_set_device(s2, d2) < 0) {
+		fprintf(stderr, "move failed\n");
 		return -1;
 	}
 
-	buf2 = sicm_arena_alloc(s2, 4092);
-
-	for(int i = 0; i < N; i++) {
-		sicm_free(buf1[i]);
-	}
-
-	sicm_free(buf2);
-	buf2 = sicm_arena_alloc(s1, 8*1024*1024*1024LL);
+	buf2 = sicm_arena_alloc(s1, 4*1024*1024*1024LL);
 	if (buf2 == NULL) {
 		fprintf(stderr, "second huge alloc failed\n");
 		return -1;
 	}
+
+	printf("so far so good buf1 %p buf2 %p\n", buf1, buf2);
+//	sleep(60);
 
 	printf("moving second huge alloc (s1) to device 2...\n");
 	if (sicm_arena_set_device(s1, d2) < 0) {
@@ -72,6 +59,7 @@ int main() {
 		return -1;
 	}
 
+	printf("sleeping\n");
 	sleep(5000);
  
 	return 0;
