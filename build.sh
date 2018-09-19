cfc0e190ea8145efe954711611c3f11e63f487df
Ben Olson <macslayer@ecuador.eecs.utk.edu>
2018-09-19 16:41:42 -0400

    I'm dumb, fixed

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ DIR=`readlink -f ./deps`
 make uninstall || true
 make distclean || true
 ./autogen.sh
-./configure --prefix=$DIR --with-jemalloc=$DIR --with-llvm=$(llvm-config-4.0 --prefix) --with-libpfm=$DIR
+./configure --prefix=$DIR --with-jemalloc=$DIR/jemalloc --with-llvm=$(llvm-config-4.0 --prefix)
 make -j5
 make install
