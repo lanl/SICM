b0e9d2f9d7a19a109c3d3a604bf1a9fbad0e0dca
Ben Olson <macslayer@argentina.eecs.utk.edu>
2018-11-29 14:04:23 -0500

    Fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ export SICM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. &&
 make uninstall || true
 make distclean || true
 ./autogen.sh
-./configure --prefix=$SICM_DIR/deps --with-jemalloc=$SICM_DIR/deps/jemalloc --with-llvm=$($SICM_DIR/deps/bin/llvm-config --prefix) --with-libpfm=$SICM_DIR/deps
+./configure --prefix=$SICM_DIR/deps --with-jemalloc=$SICM_DIR/deps --with-llvm=$($SICM_DIR/deps/bin/llvm-config --prefix) --with-libpfm=$SICM_DIR/deps
 make -j5
 make install
