dcc17777d0f941fe91e82a54e59e788e8a93c8b3
Ben Olson <macslayer@peru4.eecs.utk.edu>
2019-03-07 16:41:31 -0500

    removing debugging statements

--- src/low/sicm_arena.c
+++ src/low/sicm_arena.c
@@ -309,16 +309,9 @@ void *sicm_arena_alloc_aligned(sicm_arena a, size_t sz, size_t align) {
 	sa = a;
 	flags = 0;
 	if (sa != NULL) {
-    printf("Setting flags.\n");
-    printf("Arena index: %u\n", sa->arena_ind);
-    printf("%d\n", MALLOCX_ARENA(sa->arena_ind));
-    printf("%d\n", MALLOCX_ALIGN(align));
-    printf("%d\n", MALLOCX_TCACHE_NONE);
-    //flags = MALLOCX_ALIGN(align) | MALLOCX_ARENA(sa->arena_ind) | MALLOCX_TCACHE_NONE;
-		flags = MALLOCX_ARENA(sa->arena_ind) | MALLOCX_TCACHE_NONE;
+    flags = MALLOCX_ALIGN(align) | MALLOCX_ARENA(sa->arena_ind) | MALLOCX_TCACHE_NONE;
   }
 
-  printf("Calling je_mallocx with flags: %d\n", flags);
 	ret = je_mallocx(sz, flags);
 	return ret;
 }
