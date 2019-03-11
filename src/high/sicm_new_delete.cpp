2290d52ccef0029db65d0ae91d8abd7537ca6e5b
Ben Olson <macslayer@peru2.eecs.utk.edu>
2019-03-11 15:11:11 -0400

    bugfixin

--- src/high/sicm_new_delete.cpp
+++ src/high/sicm_new_delete.cpp
@@ -16,28 +16,28 @@ void  operator delete(void *ptr, const std::nothrow_t &) noexcept __attribute__(
 void  operator delete[](void *ptr, const std::nothrow_t &) noexcept __attribute__((used)) __attribute__((noinline));
 
 /* Call sh_alloc from all of these */
-void  *operator new(std::size_t size) __attribute__((used)) __attribute__((noinline)) {
+void  *operator new(std::size_t size){
   return sh_alloc(0, size);
 }
-void  *operator new[](std::size_t size) __attribute__((used)) __attribute__((noinline)) {
+void  *operator new[](std::size_t size){
   return sh_alloc(0, size);
 }
-void  *operator new(std::size_t size, const std::nothrow_t &) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  *operator new(std::size_t size, const std::nothrow_t &){
   return sh_alloc(0, size);
 }
-void  *operator new[](std::size_t size, const std::nothrow_t &) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  *operator new[](std::size_t size, const std::nothrow_t &){
   return sh_alloc(0, size);
 }
-void  operator delete(void *ptr) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  operator delete(void *ptr){
   sh_free(ptr);
 }
-void  operator delete[](void *ptr) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  operator delete[](void *ptr){
   sh_free(ptr);
 }
-void  operator delete(void *ptr, const std::nothrow_t &) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  operator delete(void *ptr, const std::nothrow_t &){
   sh_free(ptr);
 }
-void  operator delete[](void *ptr, const std::nothrow_t &) noexcept __attribute__((used)) __attribute__((noinline)) {
+void  operator delete[](void *ptr, const std::nothrow_t &){
   sh_free(ptr);
 }
 
