463975956d3a5470705d5a9b97317d2128ba4c2a
Ben Olson <molson5@vols.utk.edu>
2019-07-23 16:46:09 -0700

    Debugging

--- src/high/sicm_profile.c
+++ src/high/sicm_profile.c
@@ -16,7 +16,9 @@ void setup_timer(profile_thread *pt) {
   /* We need to wait until the thread has initialized
    * its tid */
   printf("Waiting for a tid in %p to be defined...\n", pt);
-  while(pt->tid == NULL) {}
+  while(pt->tid == NULL) {
+    printf("Still waiting: %p\n", pt->tid);
+  }
   printf("Finished waiting.\n");
 
   /* Set up the signal handler */
