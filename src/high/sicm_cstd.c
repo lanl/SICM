c4a73aa2bacc9d899617790f497e1cbbef571930
Ben Olson <molson5@vols.utk.edu>
2019-08-21 07:34:28 -0400

    Defining ,               total        used        free      shared  buff/cache   available Mem:      196738872     1051424   194728048       18364      959400   194433816 Swap:       9764860           0     9764860, etc.

--- src/high/sicm_cstd.c
+++ src/high/sicm_cstd.c
@@ -4,6 +4,28 @@
 #include <string.h>
 #include "sicm_high.h"
 
+/* Just define `malloc`, `calloc`, `realloc`, and `free`. We want
+ * all allocations to come through us no matter what, else we'll have edge cases
+ * where a stdlib call allocates memory without having been transformed by our compiler
+ * pass. In some cases this results in a `malloc` call in a shared library (which uses `libc`'s `malloc`),
+ * and an inlined `free` call which gets transformed by our compiler wrappers.
+ */
+void *malloc(size_t size) {
+  return sh_alloc(0, size);
+}
+
+void *calloc(size_t num, size_t size) {
+  return sh_calloc(0, num, size);
+}
+
+void *realloc(void *ptr, size_t new_size) {
+  return sh_realloc(0, ptr, new_size);
+}
+
+void free(void *ptr) {
+  sh_free(ptr);
+}
+
 /* I'm commenting these out because external libraries (such as libpfm4) will
  * use these function to allocate memory, then use `libc`'s `free` to free up the
  * memory allocated with them, resulting in an invalid pointer (since the pointer
