4888584fe7e0741fea964193e63e85f49936f180
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-09-12 13:59:48 -0700

    auto commit from rebuild.sh Thu Sep 12 13:59:48 PDT 2019

--- src/high/graph/sicm_layout.c
+++ src/high/graph/sicm_layout.c
@@ -282,9 +282,14 @@ static int parse_attr(parse_info *info, sicm_layout_node_ptr current_node, const
 static int parse_int_value(parse_info *info, sicm_layout_node_ptr current_node, const char *kwd, long int *integer) {
     int line;
 
-    LOG("!!! %s\n", kwd);
+    LOG("!!!1 %s\n", kwd);
 
-    if ((line = optional_keyword(&info, kwd))) {
+    line = optional_keyword(&info, kwd);
+
+    LOG("!!!2 %s\n", kwd);
+    if (line) {
+
+/*     if ((line = optional_keyword(&info, kwd))) { */
         if (!current_node) {
             parse_error_l(&info, line, "can't set '%s' for unspecified node\n", kwd);
         }
