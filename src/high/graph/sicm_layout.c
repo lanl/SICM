12073cf4bb0fffaeea6cbb121d48e7faa9b670cf
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-09-20 12:42:17 -0700

    auto commit from rebuild.sh Fri Sep 20 12:42:17 PDT 2019

--- src/high/graph/sicm_layout.c
+++ src/high/graph/sicm_layout.c
@@ -730,7 +730,7 @@ struct sicm_device * sl_node_device(sl_node_handle handle) {
     }
 
     for (i = 0; i < layout.device_list.count; i += 1) {
-        device = layout.device_list.devices + i;
+        device = layout.device_list.devices + i
         if (device->tag == desired_tag && sicm_numa_id(device) == node->numa_node_id) {
             return device;
         }
