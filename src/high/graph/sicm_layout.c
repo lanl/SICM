231328e0c6fc2ae744317fa979cbef716f188375
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-09-20 12:39:07 -0700

    auto commit from rebuild.sh Fri Sep 20 12:39:07 PDT 2019

--- src/high/graph/sicm_layout.c
+++ src/high/graph/sicm_layout.c
@@ -712,6 +712,30 @@ int sl_node_is_gpu(sl_node_handle handle) {
 }
 
 sicm_device * sl_node_device(sl_node_handle handle) {
+    sl_node_ptr  node;
+    int          i;
+    sicm_device *device;
+    int          desired_tag;
+
+    node = find_existing_node(handle);
+
+    if (node->kind == SL_NODE_COMPUTE)    { return NULL; }
+
+    if (node->attrs & SL_NODE_HBM) {
+        desired_tag = SICM_KNL_HBM;
+    } else if (node->attrs & SL_NODE_NVM) {
+        desired_tag = SICM_KNL_HBM;
+    } else {
+        desired_tag = SICM_DRAM;
+    }
+
+    for (i = 0; i < layout.device_list.count; i += 1) {
+        device = layout.device_list.devices[i];
+        if (device->tag == desired_tag && sicm_numa_id(device) == node->numa_node_id) {
+            return device;
+        }
+    }
+
     return NULL;
 }
 
