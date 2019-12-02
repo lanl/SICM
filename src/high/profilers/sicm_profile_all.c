a1ed29f87c8eb0446f57114718bb182479f296b7
Ben Olson <molson5@vols.utk.edu>
2019-12-02 18:25:40 -0500

    Debugging

--- src/high/profilers/sicm_profile_all.c
+++ src/high/profilers/sicm_profile_all.c
@@ -197,6 +197,7 @@ void profile_all_interval(int s) {
       aprof->profile_all.tmp_accumulator = 0;
     }
 
+#if 0
     /* Wait for the perf buffer to be ready */
     pfd.fd = prof.profile_all.fds[i];
     pfd.events = POLLIN;
@@ -211,6 +212,7 @@ void profile_all_interval(int s) {
       fprintf(stderr, "Error occurred polling. Aborting.\n");
       exit(1);
     }
+#endif
 
     /* Get ready to read */
     head = prof.profile_all.metadata[i]->data_head;
