4e1466e26b464fbbf7490dd7d3bffb2d0ba55ab9
Jason Lee <jasonlee@lanl.gov>
2021-02-25 11:09:50 -0700

    remove C++03 allocators and combine allocator tests
    replaced SICMArenaAllocator cleanup tracker with std::shared_ptr
    removed allocator tests and replaced them with executable in examples

--- include/low/public/sicm.hpp
+++ include/low/public/sicm.hpp
@@ -6,12 +6,7 @@
 // which is licesned under CC BY 4.0
 // (https://creativecommons.org/licenses/by/4.0/)
 
-#if __cplusplus < 201103L     // C++03 and backward
-#include "sicm-arena-03.hpp"
-#include "sicm-device-03.hpp"
-#elif __cplusplus >= 201103L  // C++11 and forward
-#include "sicm-arena-11.hpp"
-#include "sicm-device-11.hpp"
-#endif
+#include "sicm_arena_allocator.hpp"
+#include "sicm_device_allocator.hpp"
 
 #endif
