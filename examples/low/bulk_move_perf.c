830e21f6df77fc0866521271d23353e21a64a674
Jason Lee <jasonlee@lanl.gov>
2021-04-06 13:37:38 -0600

    change bulk_move_perf to use a common function

--- examples/low/bulk_move_perf.c
+++ examples/low/bulk_move_perf.c
@@ -10,9 +10,18 @@
 #include "sicm_low.h"
 #include "sizes.h"
 
-void *malloc_thread(void *ptr) {
-    struct ThreadArgs *args = ptr;
+int numa_move_pages_wrapper(void **ptrs, size_t count, int *dsts, int *statuses,
+                            sicm_arena arena, sicm_device *dst) {
+    return numa_move_pages(0, count, ptrs, dsts, statuses, 0);
+}
 
+void *thread_common(struct ThreadArgs *args,
+                    sicm_arena (*CREATE_ARENA)(sicm_device *, sicm_device_list *),
+                    void *(*ALLOC)(const size_t, int, sicm_arena),
+                    int (*ALLOC_MOVE)(void **, size_t, int *, int *, sicm_arena, sicm_device *),
+                    int (*MOVE)(void **, size_t, int *, int *, sicm_arena, sicm_device *),
+                    void (*FREE) (void *, const size_t),
+                    void (*DESTROY_ARENA)(sicm_arena)) {
     if (pin_thread(args->id) != 0) {
         fprintf(stderr, "Could not pin thread to cpu %zu\n", args->id);
         return NULL;
@@ -26,12 +35,18 @@ void *malloc_thread(void *ptr) {
     int *nodes = numa_alloc_onnode(int_size, 0);
     int *statuses = numa_alloc_onnode(int_size, 0);
 
+    /* only used by SICM */
+    sicm_device_list srcs;
+    sicm_arena arena;
+    if (CREATE_ARENA) {
+        arena = CREATE_ARENA(args->src, &srcs);
+    }
+
     /* allocate */
     for(size_t i = 0; i < args->allocations; i++) {
         nodes[i] = args->src->node;
 
-        ptrs[i] = malloc(args->sizes[i]);
-
+        ptrs[i] = ALLOC(args->sizes[i], nodes[i], arena);
         if (!ptrs[i]) {
             fprintf(stderr, "Could not allocate ptrs[%zu]\n", i);
         }
@@ -39,31 +54,41 @@ void *malloc_thread(void *ptr) {
         memset(ptrs[i], 0, args->sizes[i]);
     }
 
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
+    /* move ptrs to source NUMA node */
+    if (ALLOC_MOVE) {
+        if (numa_move_pages_wrapper(ptrs, args->allocations, nodes, statuses, NULL, NULL) != 0) {
+            const int err = errno;
+            fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
+            return NULL;
+        }
     }
 
+    /* change array to send to dst NUMA node */
     for(size_t i = 0; i < args->allocations; i++) {
         nodes[i] = args->dst->node;
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &args->start);
-
     /* bulk move */
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
-    }
-
+    clock_gettime(CLOCK_MONOTONIC, &args->start);
+    const int rc = MOVE(ptrs, args->allocations, nodes, statuses, arena, args->dst);
     clock_gettime(CLOCK_MONOTONIC, &args->end);
 
+    if (rc != 0) {
+        const int err = errno;
+        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
+    }
+
     /* free */
-    for(size_t i = 0; i < args->allocations; i++) {
-        free(ptrs[i]);
-        ptrs[i] = NULL;
+    if (FREE) {
+        for(size_t i = 0; i < args->allocations; i++) {
+            FREE(ptrs[i], args->sizes[i]);
+            ptrs[i] = NULL;
+        }
+    }
+
+    /* only used by SICM */
+    if (DESTROY_ARENA) {
+        DESTROY_ARENA(arena);
     }
 
     numa_free(statuses, int_size);
@@ -71,177 +96,86 @@ void *malloc_thread(void *ptr) {
     numa_free(ptrs, ptr_size);
 
     return NULL;
+}
+
+void *alloc_malloc(const size_t size, int src, sicm_arena arena) {
+    return malloc(size);
+}
+
+void free_malloc(void *ptr, const size_t size) {
+    free(ptr);
+}
+
+void *malloc_thread(void *ptr) {
+    return thread_common(ptr,
+                         NULL,
+                         alloc_malloc, numa_move_pages_wrapper,
+                         numa_move_pages_wrapper,
+                         free_malloc,
+                         NULL);
+}
+
+void *alloc_mmap(const size_t size, int src, sicm_arena arena) {
+    return mmap(NULL, size, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+}
+
+void free_mmap(void *ptr, const size_t size) {
+    munmap(ptr, size);
 }
 
 void *mmap_thread(void *ptr) {
-    struct ThreadArgs *args = ptr;
+    return thread_common(ptr,
+                         NULL,
+                         alloc_mmap, numa_move_pages_wrapper,
+                         numa_move_pages_wrapper,
+                         free_mmap,
+                         NULL);
+}
 
-    if (pin_thread(args->id) != 0) {
-        fprintf(stderr, "Could not pin thread to cpu %zu\n", args->id);
-        return NULL;
-    }
+void *alloc_numa(const size_t size, int src, sicm_arena arena) {
+    return numa_alloc_onnode(size, src);
+}
 
-    const size_t ptr_size = args->allocations * sizeof(void *);
-    void **ptrs = numa_alloc_onnode(ptr_size, 0);
-    memset(ptrs, 0, ptr_size);
-
-    const size_t int_size = args->allocations * sizeof(int);
-    int *nodes = numa_alloc_onnode(int_size, 0);
-    int *statuses = numa_alloc_onnode(int_size, 0);
-
-    /* allocate */
-    for(size_t i = 0; i < args->allocations; i++) {
-        nodes[i] = args->src->node;
-
-        ptrs[i] = mmap(NULL, args->sizes[i], PROT_READ | PROT_WRITE,
-                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-        if (!ptrs[i]) {
-            fprintf(stderr, "Could not allocate ptrs[%zu]\n", i);
-        }
-
-        memset(ptrs[i], 0, args->sizes[i]);
-    }
-
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
-    }
-
-    for(size_t i = 0; i < args->allocations; i++) {
-        nodes[i] = args->dst->node;
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &args->start);
-
-    /* bulk move */
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &args->end);
-
-    /* free */
-    for(size_t i = 0; i < args->allocations; i++) {
-        munmap(ptrs[i], args->sizes[i]);
-        ptrs[i] = NULL;
-    }
-
-    numa_free(statuses, int_size);
-    numa_free(nodes, int_size);
-    numa_free(ptrs, ptr_size);
-
-    return NULL;
+void free_numa(void *ptr, const size_t size) {
+    numa_free(ptr, size);
 }
 
 void *numa_thread(void *ptr) {
-    struct ThreadArgs *args = ptr;
+    return thread_common(ptr,
+                         NULL,
+                         alloc_numa, NULL,
+                         numa_move_pages_wrapper,
+                         free_numa,
+                         NULL);
+}
 
-    if (pin_thread(args->id) != 0) {
-        fprintf(stderr, "Could not pin thread to cpu %zu\n", args->id);
-        return NULL;
-    }
+sicm_arena create_arena_sicm(sicm_device *src, sicm_device_list *srcs) {
+    srcs->count = 1;
+    srcs->devices = &src;
+    return sicm_arena_create(0, 0, srcs);
+}
 
-    const size_t ptr_size = args->allocations * sizeof(void *);
-    void **ptrs = numa_alloc_onnode(ptr_size, 0);
-    memset(ptrs, 0, ptr_size);
+void *alloc_sicm(const size_t size, int src, sicm_arena arena) {
+    return sicm_arena_alloc(arena, size);
+}
 
-    const size_t int_size = args->allocations * sizeof(int);
-    int *nodes = numa_alloc_onnode(int_size, 0);
-    int *statuses = numa_alloc_onnode(int_size, 0);
+int move_sicm(void **ptrs, size_t count, int *dsts, int *statuses,
+              sicm_arena arena, sicm_device *dst) {
+    return sicm_arena_set_device(arena, dst);
+}
 
-    /* allocate */
-    for(size_t i = 0; i < args->allocations; i++) {
-        nodes[i] = args->src->node;
-
-        ptrs[i] = numa_alloc_onnode(args->sizes[i], nodes[i]);
-
-        if (!ptrs[i]) {
-            fprintf(stderr, "Could not allocate ptrs[%zu]\n", i);
-        }
-
-        memset(ptrs[i], 0, args->sizes[i]);
-    }
-
-    for(size_t i = 0; i < args->allocations; i++) {
-        nodes[i] = args->dst->node;
-    }
-
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &args->start);
-
-    /* bulk move */
-    if (numa_move_pages(0, args->allocations, ptrs, nodes, statuses, 0) != 0) {
-        const int err = errno;
-        fprintf(stderr, "Move pages to src failed: %s %d\n", strerror(err), err);
-        return NULL;
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &args->end);
-
-    /* free */
-    for(size_t i = 0; i < args->allocations; i++) {
-        numa_free(ptrs[i], args->sizes[i]);
-        ptrs[i] = NULL;
-    }
-
-    numa_free(statuses, int_size);
-    numa_free(nodes, int_size);
-    numa_free(ptrs, ptr_size);
-
-    return NULL;
+void destroy_arena_sicm(sicm_arena arena) {
+    sicm_arena_destroy(arena);
 }
 
 void *sicm_thread(void *ptr) {
-    struct ThreadArgs *args = ptr;
-
-    if (pin_thread(args->id) != 0) {
-        fprintf(stderr, "Could not pin thread to cpu %zu\n", args->id);
-        return NULL;
-    }
-
-    const size_t ptr_size = args->allocations * sizeof(void *);
-    void **ptrs = numa_alloc_onnode(ptr_size, 0);
-    memset(ptrs, 0, ptr_size);
-
-    sicm_device_list srcs;
-    srcs.count = 1;
-    srcs.devices = &args->src;
-
-    sicm_arena arena = sicm_arena_create(0, 0, &srcs);
-
-    /* allocate */
-    for(size_t i = 0; i < args->allocations; i++) {
-        ptrs[i] = sicm_arena_alloc(arena, args->sizes[i]);
-
-        if (!ptrs[i]) {
-            fprintf(stderr, "Could not allocate ptrs[%zu]\n", i);
-        }
-
-        memset(ptrs[i], 0, args->sizes[i]);
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &args->start);
-
-    /* bulk move */
-    sicm_arena_set_device(arena, args->dst);
-
-    clock_gettime(CLOCK_MONOTONIC, &args->end);
-
-    /* frees all pointers in the arena */
-    sicm_arena_destroy(arena);
-
-    numa_free(ptrs, ptr_size);
-
-    return NULL;
+    return thread_common(ptr,
+                         create_arena_sicm,
+                         alloc_sicm, NULL,
+                         move_sicm,
+                         NULL,
+                         destroy_arena_sicm);
 }
 
 int main(int argc, char *argv[]) {
