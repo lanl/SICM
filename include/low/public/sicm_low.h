be2828ddee45319a92aa2b6a827f5675214677ea
Jason Lee <jasonlee@lanl.gov>
2021-11-02 18:32:01 -0500

    Add HIP device detection

--- include/low/public/sicm_low.h
+++ include/low/public/sicm_low.h
@@ -19,6 +19,10 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+#ifdef HIP
+#include <hip/hip_runtime.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -36,6 +40,7 @@ typedef enum sicm_device_tag {
   SICM_KNL_HBM,
   SICM_POWERPC_HBM,
   SICM_OPTANE,
+  SICM_HIP,
   INVALID_TAG
 } sicm_device_tag;
 
@@ -69,6 +74,14 @@ typedef struct sicm_optane_data {
   int compute_node;
 } sicm_optane_data;
 
+#ifdef HIP
+/// Data specific to a HIP device.
+typedef struct sicm_hip_data {
+    int compute_node;
+    hipDeviceProp_t prop;
+} sicm_hip_data;
+#endif
+
 /// Data that, given a device type, uniquely identify the device within that type.
 /**
  * This union is only meaningful in the presence of a sicm_device_tag,
@@ -81,6 +94,9 @@ typedef union sicm_device_data {
   sicm_knl_hbm_data knl_hbm;
   sicm_powerpc_hbm_data powerpc_hbm;
   sicm_optane_data optane;
+#ifdef HIP
+  sicm_hip_data hip;
+#endif
 } sicm_device_data;
 
 /// Tagged/discriminated union that fully identifies a device.
---
+++ src/low/detect_devices/HIP.c
@@ -0,0 +1,86 @@
+#include "detect_devices/HIP.h"
+#include <stdio.h>
+
+#ifdef HIP
+#include <hip/hip_runtime.h>
+#endif
+
+int get_HIP_node_count() {
+    int usable = 0;
+
+    #ifdef HIP
+    hipInit(0);
+
+    int dev_count = 0;
+    hipGetDeviceCount(&dev_count);
+
+    for(int i = 0; i < dev_count; i++) {
+        hipDeviceProp_t prop;
+        hipGetDeviceProperties(&prop, i);
+
+        if (!prop.canMapHostMemory ||
+            !prop.totalGlobalMem) {
+            continue;
+        }
+
+        usable++;
+    }
+    #endif
+
+    return usable;
+}
+
+#ifdef HIP
+static int get_numa_node(int pciBusID) {
+    int numa_node = -1;
+    char filename[PATH_MAX];
+    snprintf(filename, PATH_MAX, "/sys/class/pci_bus/0000:%02x/device/numa_node", pciBusID);
+    FILE *f = fopen(filename, "r");
+    if (f) {
+        // ignore errors
+        fscanf(f, "%d", &numa_node);
+    }
+    fclose(f);
+    return numa_node;
+}
+#endif
+
+void detect_HIP(struct bitmask* compute_nodes, struct bitmask* non_dram_nodes,
+                int *huge_page_sizes, int huge_page_size_count, int normal_page_size,
+                struct sicm_device **devices, int *curr_idx) {
+    #ifdef HIP
+    int idx = *curr_idx;
+
+    int dev_count = 0;
+    hipGetDeviceCount(&dev_count);
+
+    for(int i = 0; i < dev_count; i++) {
+        hipDeviceProp_t prop;
+        hipGetDeviceProperties(&prop, i);
+
+        if (!prop.canMapHostMemory ||
+            !prop.totalGlobalMem) {
+            continue;
+        }
+
+        const int numa_node = get_numa_node(prop.pciBusID);
+        const int compute_node = numa_bitmask_isbitset(compute_nodes, numa_node);
+
+        devices[idx]->tag = SICM_HIP;
+        devices[idx]->node = numa_node;
+        devices[idx]->page_size = normal_page_size;
+        devices[idx]->data.hip = (struct sicm_hip_data){ .compute_node = compute_node, .prop = prop };
+        // do not modify non_dram_nodes since HIP devices might be co-located with DRAM
+        idx++;
+        for(int j = 0; j < huge_page_size_count; j++) {
+            devices[idx]->tag = SICM_HIP;
+            devices[idx]->node = numa_node;
+            devices[idx]->page_size = huge_page_sizes[j];
+            devices[idx]->data.hip = (struct sicm_hip_data){ .compute_node = compute_node, .prop = prop };
+            idx++;
+        }
+    }
+
+    *curr_idx = idx;
+    #endif
+}
