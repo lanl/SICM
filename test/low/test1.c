fa7fabd946d1ed24dec8f2406d8db3449c3bdc85
Jason Lee <jasonlee@lanl.gov>
2022-02-04 12:40:34 -0500

    update test1 to allocate on all devices
    also write to allocations

--- test/low/test1.c
+++ test/low/test1.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #include <sicm_low.h>
 
-sicm_device_list devs;
-
 int main() {
-	int i;
-	sicm_arena s;
-	char *buf1, *buf2;
-	sicm_device_list ds;
+	sicm_device_list devs = sicm_init();
 
-	devs = sicm_init();
-	for(i = 0; i < devs.count; i++) {
-		printf("%d %d\n", i, sicm_numa_id(devs.devices[i]));
+	for(int i = 0; i < devs.count; i++) {
+		sicm_device *dev = devs.devices[i];
+		printf("%d %d %s %d\n", i, sicm_numa_id(dev), sicm_device_tag_str(dev->tag), dev->page_size);
 	}
 
-	ds.count = 1;
-	ds.devices = &devs.devices[0];
-	s = sicm_arena_create(0, 0, &ds);
-	if (s == NULL) {
-		fprintf(stderr, "sicm_arena_create failed\n");
-		return -1;
+	for(int i = 0; i < devs.count; i++) {
+		sicm_device *dev = devs.devices[i];
+		sicm_device_list ds = {
+			.count = 1,
+			.devices = &dev,
+		};
+
+		sicm_arena s = sicm_arena_create(0, 0, &ds);
+		if (s == NULL) {
+			fprintf(stderr, "sicm_arena_create failed for device %d: %d %d %s (%d)\n",
+					i, sicm_numa_id(dev), dev->page_size,
+					sicm_device_tag_str(dev->tag), dev->tag);
+			return -1;
+		}
+
+		char *buf1 = sicm_arena_alloc(s, 1024);
+		char *buf2 = sicm_arena_alloc(s, 2048*1024);
+		memset(buf1, 0, 1024);
+		memset(buf2, 0, 2048*1024);
+		sicm_free(buf2);
+		sicm_free(buf1);
+
+		sicm_arena_destroy(s);
 	}
 
-	buf1 = sicm_arena_alloc(s, 1024);
-	buf2 = sicm_arena_alloc(s, 2048*1024);
-	sicm_free(buf1);
-	sicm_free(buf2);
-
-	sicm_arena_destroy(s);
 	sicm_fini();
-
 	return 0;
 }
