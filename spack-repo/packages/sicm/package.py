2db58d98438ad7e27631e6efbbd0d460a6ba0e2f
Jason Lee <jasonlee@lanl.gov>
2022-04-14 10:09:03 -0600

    spack package updates
    renamed sicm-low to sicm
    removed sicm-high due to compilation errors
    sicm-high should be added back as a variant

--- spack-repo/packages/sicm/package.py
+++ spack-repo/packages/sicm/package.py
@@ -1,18 +1,18 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-import os
 
 
-class SicmLow(CMakePackage):
-    """SICM: Simplified Interface to Complex Memory.
-    Includes only the low-level interface and arena allocator."""
+class Sicm(CMakePackage):
+    """SICM: Simplified Interface to Complex Memory."""
 
     homepage = "https://github.com/lanl/SICM/"
     git      = "https://github.com/lanl/SICM.git"
+
+    maintainers = []
 
     version('master')
 
@@ -20,5 +20,4 @@ class SicmLow(CMakePackage):
     depends_on('numactl')
 
     def cmake_args(self):
-        args = []
-        return args
+        return []
